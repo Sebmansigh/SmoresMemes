92d6eb0126573bc2f93be7394a50b07268079c7d
Sebastian Snyder <sebastiansnyder@yahoo.com>
2018-02-05 21:12:41 -0600

    Reading from the database is now also a thing you can do!

--- app/src/main/java/com/cs316/smoresmemes/MainActivity.java
+++ app/src/main/java/com/cs316/smoresmemes/MainActivity.java
@@ -3,21 +3,63 @@ package com.cs316.smoresmemes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        final Map<String,String> Data = new HashMap<>();
+        Data.put("arg1","?%=!");
+        Data.put("arg2","Words");
+        Data.put("arg3","?%=!");
+        Data.put("arg4","WordsAgain!");
         Runnable c = new Runnable() {
             @Override
             public void run() {
-                String X = MyHTTP.POST("HI!");
+                String X = MyHTTP.POST("test",Data);
                 System.out.print("RESULTS: ");
                 System.out.println(X);
             }
         };
+        //*/
+        /*
+        final Map<String,String> Data = new HashMap<>();
+        Data.put("path","pathnameLOL");
+
+        Runnable c = new Runnable() {
+            @Override
+            public void run() {
+                String X = MyHTTP.POST("postmeme",Data);
+                System.out.print("RESULTS: ");
+                System.out.println(X);
+            }
+        };
+        //*/
+        /*
+        final Map<String,String> Data = new HashMap<>();
+        Data.put("id","2");
+
+        Runnable c = new Runnable() {
+            @Override
+            public void run() {
+                String X = MyHTTP.POST("getmeme",Data);
+                System.out.print("RESULTS: ");
+                System.out.println(X);
+            }
+        };
+        //*/
+
+        /*
+        //To test the queries, uncomment one of the above comments and this one.
         System.out.println("RUNNING!");
         new Thread(c).start();
+        //*/
     }
 }
--- app/src/main/java/com/cs316/smoresmemes/MyHTTP.java
+++ app/src/main/java/com/cs316/smoresmemes/MyHTTP.java
@@ -10,6 +10,7 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 /**
  * Created by sebmansigh on 2/5/18.
@@ -19,14 +20,26 @@ public final class MyHTTP {
     private MyHTTP() {
     }
 
-    ;
+    public static String Encode(String data) {
+        return data.replace("!", "%21")
+                .replace("&", "%26")
+                .replace("'", "%27")
+                .replace("/", "%2F")
+                .replace(":", "%3A")
+                .replace("=", "%3D")
+                .replace("?", "%3F");
 
-    public static String POST(String data) {
+    }
+
+    /*!	#	$	&	'	(	)	*	+	,	/	:	;	=	?	@	[	]
+    %21	%23	%24	%26	%27	%28	%29	%2A	%2B	%2C	%2F	%3A	%3B	%3D	%3F	%40	%5B	%5D
+    */
+    public static String POST(String task, Map<String, String> args) {
         URL url = null;
         try {
-            url = new URL("http://cs316smoresmemes.online/test.php");
+            url = new URL("http://cs316smoresmemes.online/" + task + ".php");
         } catch (MalformedURLException e) {
-            return e.getMessage();
+            return e.getClass().getName() + "1: " + e.getMessage() + "\n";
         }
 
         try {
@@ -35,7 +48,10 @@ public final class MyHTTP {
             connection.setRequestMethod("POST");
             connection.connect();
             OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
-            wr.write("path=" + data);
+            for (Map.Entry<String, String> arg : args.entrySet()) {
+                String qstr = Encode(arg.getKey()) + "=" + Encode(arg.getValue()) + "&";
+                wr.write(qstr);
+            }
             wr.flush();
             wr.close();
 
@@ -46,15 +62,8 @@ public final class MyHTTP {
             }
 
             return content;
-            /*
-                URLConnection conn = url.openConnection();
-                conn.setDoOutput(true);
-                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-                wr.write( data );
-                wr.flush();
-            */
         } catch (Exception e) {
-            return e.getMessage();
+            return e.getClass().getName() + "2: " + e.getMessage();
         }
     }
 }
