247b040edabb09bf8eee2eec48029edbe9cff1ed
Carlos Velazquez <cxv13a@acu.edu>
2018-02-12 15:57:12 -0600

    Camera works?

--- app/src/main/java/com/cs316/smoresmemes/MainActivity.java
+++ app/src/main/java/com/cs316/smoresmemes/MainActivity.java
@@ -1,7 +1,15 @@
 package com.cs316.smoresmemes;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -9,11 +17,23 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView photoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton btnCamera = (ImageButton) findViewById(R.id.imageButton2);
+        ImageView photoView = (ImageView)findViewById(R.id.imageView);
+        System.out.println("PV: "+photoView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
         /*
         final Map<String,String> Data = new HashMap<>();
         Data.put("arg1","?%=!");
@@ -59,5 +79,14 @@ public class MainActivity extends AppCompatActivity {
         new Thread(c).start();
         new Thread(d).start();
         */
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        ImageView photoView = (ImageView)findViewById(R.id.imageView);
+        System.out.println("PV: "+photoView);
+        photoView.setImageBitmap(bitmap);
     }
 }
